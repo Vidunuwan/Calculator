df955ef5bd941d9599e317a0df9fc74e5bfef78e
Vidunuwan <uvidunuwan@gmail.com>
2021-08-23 01:57:53 +0530

    v2 final code

--- src/java/com/kelaniya/uni/v1/Main.java
+++ src/java/com/kelaniya/uni/v1/Main.java
@@ -1,7 +1,7 @@
 package com.kelaniya.uni.v1;
 
 public class Main {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         System.out.println("a");
 
     }
--- src/java/com/kelaniya/uni/v2/Main.java
+++ src/java/com/kelaniya/uni/v2/Main.java
@@ -1,19 +1,45 @@
 package com.kelaniya.uni.v2;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {//exseption handling
         //make sure to validate the argumant before the using
-        if(args.length==0){
+        if (args.length == 0) {
             System.out.println("Please provide the oparation as a argumant");
             return;
         }
-        String oparator=args[0];
-        if(!(oparator.equals("add") || oparator.equals("sub") || oparator.equals("mul") ||oparator.equals("div"))){
-            System.out.println("Please provide the cuttect argumant");
+        String oparator = args[0];
+        if (!(oparator.equals("add") || oparator.equals("sub") || oparator.equals("mul") || oparator.equals("div"))) {
+            System.out.println("Please provide the correct argument");
             return;
-
         }
-        System.out.println(args[0]);
+        //read the numbers from text files
+        List<String> stringsStrs = Files.readAllLines(
+                Paths.get("D:\\My\\SE\\2 ND YEAR\\Sem 1\\" +
+                        "Software Constructions\\Git Local\\Calculator\\src\\java\\com\\kelaniya\\uni\\Numbers.txt")
+        );
+
+        double number1 = Double.parseDouble(stringsStrs.get(0));
+        double number2 = Double.parseDouble(stringsStrs.get(1));
+
+        double result = 0;
+
+        if (oparator.equals("add")) {
+            result = number1 + number2;
+        } else if (oparator.equals("sub")) {
+            result = number1 - number2;
+        } else if (oparator.equals("mul")) {
+            result = number1 * number2;
+        } else {
+            result = number1 / number2;
+        }
+        System.out.println("The result is " + result);
+
 
     }
 }
